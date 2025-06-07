344fef1eb172fe34f05a2ca7e7302b9f75fe6fee
Benjaminisgood <1093158714@qq.com>
2025-06-07 12:23:46 +0800

    画质再低一点

--- Neibr/views.py
+++ Neibr/views.py
@@ -18,7 +18,7 @@ from math import ceil
 # 项目根目录下的 static/neibr 路径
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 UPLOAD_BASE_PATH = os.path.join(BASE_DIR, 'Neibr', 'neibr')
-IMAGE_QUALITY = 75
+IMAGE_QUALITY = 66
 
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'webm', 'mp3', 'wav', 'nef','mov','MOV'}
